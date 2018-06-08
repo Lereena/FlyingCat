27888d5f6ab08a41200871b72e5963f03fc0236a
Lereena <lereenadem@yandex.ru>
2018-06-08 18:30:21 +0300

    pics updated

--- app/src/main/java/lekkermode/flyingcat/MainActivity.java
+++ app/src/main/java/lekkermode/flyingcat/MainActivity.java
@@ -12,23 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new DrawView(this));
-     //   MainActivity.Timer t = new Timer();
-      //  t.start();
     }
-
-  /*  class Timer extends CountDownTimer {
-        public Timer() {
-            super(Integer.MAX_VALUE, timerInterval);
-        }
-        @Override
-        public void onTick(long millisUntilFinished) {
-            if (lvl1.points >= 30) {
-                startActivity(new Intent(lvl1.getContext(), Level2Activity.class));
-            }
-        }
-        @Override
-        public void onFinish(){}
-    }
-**/
 }
 
